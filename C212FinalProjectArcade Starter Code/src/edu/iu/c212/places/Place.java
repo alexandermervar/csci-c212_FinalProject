580bf161fe31152f2807491101316a818e5a1178
Aidan <ajrosbergschool@gmail.com>
2021-11-29 22:48:03 -0500

    Implemented Place class

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/Place.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/Place.java
@@ -1,5 +1,18 @@
 package edu.iu.c212.places;
 
+import edu.iu.c212.Arcade;
+import edu.iu.c212.models.User;
+
 public abstract class Place {
-    
+    private String placeName;
+    private Arcade arcade;
+    private double entryFee;
+
+    abstract void onEnter(User user);
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "Location: " + placeName + "Entry fee: " + entryFee + "$ ";
+    }
 }
