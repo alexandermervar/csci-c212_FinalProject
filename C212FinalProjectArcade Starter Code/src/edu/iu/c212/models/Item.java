499c1b568b93e48bbe45f44105906b0655328b22
Aidan <ajrosbergschool@gmail.com>
2021-11-29 22:31:09 -0500

    Implemented item enum

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/Item.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/Item.java
@@ -1,5 +1,20 @@
 package edu.iu.c212.models;
 
 public enum Item {
-    
+    // TODO Create final items
+    // Test items
+    APPLE ("apple", 12.5);
+
+    String readableName;
+    double value;
+
+    Item(String readableName, double value) {
+        this.readableName = readableName;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + readableName + " Price: " + value;
+    }
 }
