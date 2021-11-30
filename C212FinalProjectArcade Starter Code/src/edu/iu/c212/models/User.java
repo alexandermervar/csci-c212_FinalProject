62d8c5e83af9344ac5973148b3305b0ce6401934
Aidan <ajrosbergschool@gmail.com>
2021-11-29 22:36:57 -0500

    Implemented User class

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/User.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/User.java
@@ -1,5 +1,35 @@
 package edu.iu.c212.models;
 
+import java.util.List;
+
 public class User {
-    
+    private String username;
+    private double balance;
+    private List<Item> inventory;
+
+    public User(String username, double balance, List<Item> inventory) {
+        this.username = username;
+        this.setBalance(balance);
+        this.setInventory(inventory); 
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Item> inventory) {
+        this.inventory = inventory;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }  
 }
