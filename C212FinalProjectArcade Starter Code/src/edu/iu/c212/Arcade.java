917c48140daaba14f5adec7f91f9b97d6ca8a7e7
Aidan <ajrosbergschool@gmail.com>
2021-11-29 23:00:23 -0500

    Implemented IArcade interface

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/Arcade.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/Arcade.java
@@ -1,5 +1,50 @@
 package edu.iu.c212;
 
-public class Arcade {
+import java.util.List;
+
+import edu.iu.c212.models.User;
+import edu.iu.c212.places.Place;
+
+public class Arcade implements IArcade{
+
+    private User currentUser;
+    private List<User> allUsers;
+    private List<Place> allPlaces;
+
+    public Arcade() {
+        currentUser = getUserOnArcadeEntry();
+        allUsers = getUserSaveDataFromFile();
+        allPlaces = getAllPlaces();
+    }
+
+    @Override
+    public List<User> getUserSaveDataFromFile() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void saveUsersToFile() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void transitionArcadeState(String newPlaceNameToGoTo) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public User getUserOnArcadeEntry() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Place> getAllPlaces() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/IArcade.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/IArcade.java
@@ -1,5 +1,19 @@
 package edu.iu.c212;
 
+import java.util.List;
+
+import edu.iu.c212.models.User;
+import edu.iu.c212.places.Place;
+
 public interface IArcade {
+    List<User> getUserSaveDataFromFile();
+
+    void saveUsersToFile();
+
+    void transitionArcadeState(String newPlaceNameToGoTo);
+
+    User getUserOnArcadeEntry();
+
+    List<Place> getAllPlaces();
 
 }
