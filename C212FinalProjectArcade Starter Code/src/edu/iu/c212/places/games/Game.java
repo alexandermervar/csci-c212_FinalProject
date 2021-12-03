f519dfa1e16f2696179906ca2ee39ff9c3fe30e5
alexandermervar <alexandermervar@gmail.com>
2021-12-02 22:02:29 -0500

    Completed 1.0 Version of Game.java

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/Game.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/Game.java
@@ -1,5 +1,9 @@
 package edu.iu.c212.places.games;
 
+import edu.iu.c212.models.User;
+
 public abstract class Game {
     
+    // The onEnter function, which takes an object of the class User as a parameter.
+    abstract void onEnter(User user);
 }
