bd9e7214338f440f84c42847bb7960eef7f7bbb3
alexandermervar <alexandermervar@gmail.com>
2021-12-02 22:21:33 -0500

    Completed Item.java enumeration and created some items

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/Item.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/Item.java
@@ -1,9 +1,22 @@
 package edu.iu.c212.models;
 
 public enum Item {
-    // TODO Create final items
-    // Test items
-    APPLE ("apple", 12.5);
+
+    TEDDYBEAR ("Teddy Bear", 100.0);
+    CANDYBAR ("Candy Bar", 50.0);
+    JOLLYRANCHERS ("Jolly Ranchers", 10.0);
+    TINYARMYMEN ("Green Army Men", 5.0)
+    HEADPHONES ("Headphones", 10000.0);
+    DOGCHEWTOY ("Dog Chew Toy", 500.0);
+    BOUNCYBALL ("Bouncy Ball", 75.0);
+    TOOTSIEROLL ("100 Tootsie Rolls", 1);
+    ANEWCAR ("A Brand New 2021 Subaru Outback", 1000000.0);
+    GOPROCAMERA ("GoPro Camera", 1000.0);
+    GIANTSTUFFEDMONKEY ("Giant Stuffed Monkey", 750.0);
+    AVERYSMALLTOYDINOSAUR ("A Very Small Toy Dinosaur (SUPER RARE)", 1000000000000000000000.0);
+    STICKYSLAPPYHANDFROMCHILDHOOD ("Sticky Slappy Hand From your Childhood", 100.0);
+    SLIME ("Slime (Imported from Nickelodeon)", 1500.0);
+    
 
     String readableName;
     double value;
@@ -15,6 +28,6 @@ public enum Item {
 
     @Override
     public String toString() {
-        return "Name: " + readableName + " Price: " + value;
+        return "Name: " + readableName + " Price: $" + value;
     }
 }
