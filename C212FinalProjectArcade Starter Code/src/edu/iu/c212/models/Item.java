e48cdca990f70000f4c124dccc5e4d86c2ba4c0b
alexandermervar <alexandermervar@gmail.com>
2021-12-02 23:18:33 -0500

    Fixed Item.java enumeration

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/Item.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/models/Item.java
@@ -2,19 +2,19 @@ package edu.iu.c212.models;
 
 public enum Item {
 
-    TEDDYBEAR ("Teddy Bear", 100.0);
-    CANDYBAR ("Candy Bar", 50.0);
-    JOLLYRANCHERS ("Jolly Ranchers", 10.0);
-    TINYARMYMEN ("Green Army Men", 5.0)
-    HEADPHONES ("Headphones", 10000.0);
-    DOGCHEWTOY ("Dog Chew Toy", 500.0);
-    BOUNCYBALL ("Bouncy Ball", 75.0);
-    TOOTSIEROLL ("100 Tootsie Rolls", 1);
-    ANEWCAR ("A Brand New 2021 Subaru Outback", 1000000.0);
-    GOPROCAMERA ("GoPro Camera", 1000.0);
-    GIANTSTUFFEDMONKEY ("Giant Stuffed Monkey", 750.0);
-    AVERYSMALLTOYDINOSAUR ("A Very Small Toy Dinosaur (SUPER RARE)", 1000000000000000000000.0);
-    STICKYSLAPPYHANDFROMCHILDHOOD ("Sticky Slappy Hand From your Childhood", 100.0);
+    TEDDYBEAR ("Teddy Bear", 100.0),
+    CANDYBAR ("Candy Bar", 50.0),
+    JOLLYRANCHERS ("Jolly Ranchers", 10.0),
+    TINYARMYMEN ("Green Army Men", 5.0),
+    HEADPHONES ("Headphones", 10000.0),
+    DOGCHEWTOY ("Dog Chew Toy", 500.0),
+    BOUNCYBALL ("Bouncy Ball", 75.0),
+    TOOTSIEROLL ("100 Tootsie Rolls", 1),
+    ANEWCAR ("A Brand New 2021 Subaru Outback", 1000000.0),
+    GOPROCAMERA ("GoPro Camera", 1000.0),
+    GIANTSTUFFEDMONKEY ("Giant Stuffed Monkey", 750.0),
+    AVERYSMALLTOYDINOSAUR ("A Very Small Toy Dinosaur (SUPER RARE)", 1000000000000000000000.0),
+    STICKYSLAPPYHANDFROMCHILDHOOD ("Sticky Slappy Hand From your Childhood", 100.0),
     SLIME ("Slime (Imported from Nickelodeon)", 1500.0);
     
 
--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/FileUtils.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/FileUtils.java
@@ -4,7 +4,9 @@ import edu.iu.c212.models.User;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileUtils {
     private static File file = new File("C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/Users.txt");
@@ -19,11 +21,28 @@ public class FileUtils {
      * @param users The total list of all users
      */
     public static void writeUserDataToFile(List<User> users) throws IOException {
+        // loop through the list of the users and write each user to the file
+        for (User user : users) {
+            String userData = user.getUsername() + "|" + user.getBalance() + "|" + user.getInventory();
+            PrintWriter out = new PrintWriter(file);
+            out.println(userData);
+        }
     }
 
     /**
      * Read user data from the file you provided above. Return a list of Users
      */
     public static List<User> getUserDataFromFile() throws IOException {
+        File in = new File("C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/Users.txt");
+        List<User> users = new java.util.ArrayList<>();
+        
+        // Read the file line by line and create a user for each line and add it to the list
+        Scanner scan = new Scanner(in);
+        while(scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] userData = line.split("\\|");
+            User user = new User(userData[0], Integer.parseInt(userData[1]), userData[2]);
+            users.add(user);
+        }
     }
 }
