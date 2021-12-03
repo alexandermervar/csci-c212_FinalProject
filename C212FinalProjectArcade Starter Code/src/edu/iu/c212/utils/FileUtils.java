a481ffa66f3051c52062059ea20faba09ded770d
alexandermervar <alexandermervar@gmail.com>
2021-12-02 23:30:20 -0500

    Finished FileUtils.java

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/FileUtils.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/FileUtils.java
@@ -1,10 +1,12 @@
 package edu.iu.c212.utils;
 
+import edu.iu.c212.models.Item;
 import edu.iu.c212.models.User;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,11 +24,12 @@ public class FileUtils {
      */
     public static void writeUserDataToFile(List<User> users) throws IOException {
         // loop through the list of the users and write each user to the file
+        PrintWriter out = new PrintWriter(file);
         for (User user : users) {
             String userData = user.getUsername() + "|" + user.getBalance() + "|" + user.getInventory();
-            PrintWriter out = new PrintWriter(file);
             out.println(userData);
         }
+        out.close();
     }
 
     /**
@@ -34,15 +37,26 @@ public class FileUtils {
      */
     public static List<User> getUserDataFromFile() throws IOException {
         File in = new File("C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/Users.txt");
-        List<User> users = new java.util.ArrayList<>();
+        List<User> users = new ArrayList<>();
         
         // Read the file line by line and create a user for each line and add it to the list
         Scanner scan = new Scanner(in);
         while(scan.hasNextLine()) {
             String line = scan.nextLine();
             String[] userData = line.split("\\|");
-            User user = new User(userData[0], Integer.parseInt(userData[1]), userData[2]);
+
+            // Create a list from the inventory string
+            String[] userInventoryString = userData[2].split(",");
+            List<Item> userInventory = new ArrayList<Item>();
+
+            for (String item : userInventoryString) {
+                Item selectedItem = Item.valueOf(item);
+                userInventory.add(selectedItem);
+            }
+
+            User user = new User(userData[0], Integer.parseInt(userData[1]), userInventory);
             users.add(user);
         }
+        return users;
     }
 }
