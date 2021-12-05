8c71359352c0156d245c97c2e118d14ded054902
Aidan <ajrosbergschool@gmail.com>
2021-12-05 13:38:50 -0500

    Implemented transitionArcadeState

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/Arcade.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/Arcade.java
@@ -1,9 +1,12 @@
 package edu.iu.c212;
 
+import java.io.IOException;
 import java.util.List;
 
 import edu.iu.c212.models.User;
 import edu.iu.c212.places.Place;
+import edu.iu.c212.places.Places;
+import edu.iu.c212.utils.FileUtils;
 
 public class Arcade implements IArcade{
 
@@ -19,32 +22,54 @@ public class Arcade implements IArcade{
 
     @Override
     public List<User> getUserSaveDataFromFile() {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            return FileUtils.getUserDataFromFile();
+        } catch (Exception e) {
+            System.exit(1);
+            return null;
+        }
     }
 
     @Override
     public void saveUsersToFile() {
-        // TODO Auto-generated method stub
-        
+        try {
+            FileUtils.writeUserDataToFile(allUsers);
+        } catch (IOException e) {
+            System.exit(1);
+        }
     }
 
     @Override
     public void transitionArcadeState(String newPlaceNameToGoTo) {
-        // TODO Auto-generated method stub
         
+        double userBalance = currentUser.getBalance();
+
+        for (Place place : allPlaces) {
+            if (place.getPlaceName().equals(newPlaceNameToGoTo)) {
+                double entryFee = place.getEntryFee();
+
+                if (userBalance < entryFee) {
+                    System.out.println("Sorry you do not have the funds to enter this place");
+                    transitionArcadeState(Places.LOBBY);
+                }
+                else {
+                    currentUser.setBalance(userBalance - entryFee);
+                    place.onEnter(currentUser);
+                }
+            }
+        } 
     }
 
     @Override
     public User getUserOnArcadeEntry() {
         // TODO Auto-generated method stub
+
         return null;
     }
 
     @Override
     public List<Place> getAllPlaces() {
-        // TODO Auto-generated method stub
-        return null;
+        return allPlaces;
     }
     
 }
