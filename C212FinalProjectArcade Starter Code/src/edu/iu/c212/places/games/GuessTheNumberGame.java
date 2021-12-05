735f23bc97e7bbcc042751ec2b9208e4c126aaa4
alexandermervar <alexandermervar@gmail.com>
2021-12-05 13:27:58 -0500

    Removed redundant entry fee removal from GuessTheNumber.java (Trivia has an entry fee of /bin/zsh so I never implemented the redundancy, there)

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/GuessTheNumberGame.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/GuessTheNumberGame.java
@@ -18,18 +18,6 @@ public class GuessTheNumberGame extends Game{
 
         System.out.println("Welcome to Guess The Number. You'll be guessing a number between 0 and 100.");
         System.out.println("You'll get $10 if you correctly guess the number within 5 tries. Otherwise, you get nothing.");
-
-        // check to see if the user has enough money to play
-        if (user.getBalance() < getEntryFee()) {
-            System.out.println("");
-            System.out.println("You don't have enough money to play this game.");
-            System.out.println("----------");
-            return;
-        }
-
-        // Save singular user to file with new balance upon entry
-        user.setBalance(user.getBalance() - getEntryFee());
-        saveSingularUser(user);
         
         // Let the user guess the number
         while (userGuess != numberToGuess && numberOfGuesses < 5) {
