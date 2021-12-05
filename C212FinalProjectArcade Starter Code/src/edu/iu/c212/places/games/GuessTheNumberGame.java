daefa0fe4594fb14fa3c82fbea57c48670cb9c8d
Aidan <ajrosbergschool@gmail.com>
2021-12-05 17:05:34 -0500

    Fixed to allow users to win game

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/GuessTheNumberGame.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/GuessTheNumberGame.java
@@ -7,10 +7,6 @@ import edu.iu.c212.places.Places;
 
 public class GuessTheNumberGame extends Game{
 
-    private int numberToGuess;
-    private int numberOfGuesses = 0;
-    private int userGuess;
-
     public GuessTheNumberGame(String placeName) {
         setPlaceName(placeName);
         setEntryFee(5.0);
@@ -20,17 +16,20 @@ public class GuessTheNumberGame extends Game{
     @Override
     public void onEnter(User user) {
 
-        numberToGuess = (int) (Math.random() * 100);
+        Scanner sc = new Scanner(System.in);
+
+        int numberToGuess = (int) (Math.random() * 5);
+        int userGuess = -1;
+        int numberOfGuesses = 0;
 
         System.out.println("Welcome to Guess The Number. You'll be guessing a number between 0 and 100.");
         System.out.println("You'll get $10 if you correctly guess the number within 5 tries. Otherwise, you get nothing.");
         
         // Let the user guess the number
         while (userGuess != numberToGuess && numberOfGuesses < 5) {
-            System.out.println("What's' your guess?");
+            System.out.println("What's your guess?");
             
-            Scanner sc = new Scanner(System.in);
-            int userGuess = Integer.parseInt(sc.nextLine());
+            userGuess = sc.nextInt();
             numberOfGuesses++;
 
             if (userGuess != numberToGuess) {
@@ -39,7 +38,7 @@ public class GuessTheNumberGame extends Game{
                 if (userGuess > numberToGuess) {
                     System.out.println("Your number was too high.");
                 } else {
-                    System.out.println("Your number was too low.");
+                    System.out.println("Your number was too low.\n");
                 }
             }
         }
@@ -47,15 +46,18 @@ public class GuessTheNumberGame extends Game{
         if (userGuess == numberToGuess) {
             System.out.println("Congratulations, you correctly guessed the number!");
             System.out.println("You guessed it within " + numberOfGuesses + " tries, so you get $10");
-            user.setBalance(user.getBalance() + getPrize());
             System.out.println("----------");
-            // saveSingularUser(user);
+
+            user.setBalance(user.getBalance() + getPrize());
+
             getArcade().saveUsersToFile();
             getArcade().transitionArcadeState(Places.LOBBY);
         } else {
             System.out.println("You didn't guess the number in time.");
             System.out.println("You lost $" + getEntryFee() + ".");
             System.out.println("----------");
+
+            getArcade().saveUsersToFile();
             getArcade().transitionArcadeState(Places.LOBBY);        }
 
     }
