f09c520e3e0a5a8f5f070ca95b1894216176fc12
alexandermervar <alexandermervar@gmail.com>
2021-12-05 10:59:52 -0500

    Finished 1.0 Version of Game.java abstract class

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/Game.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/Game.java
@@ -1,9 +1,10 @@
 package edu.iu.c212.places.games;
 
-import edu.iu.c212.models.User;
+import edu.iu.c212.utils.*;
 import edu.iu.c212.places.Place;
 
 public abstract class Game extends Place {
     
-    //TODO: Add shared functionality between games, here
+    private double prize;
+
 }
