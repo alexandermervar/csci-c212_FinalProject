8b46832b74bac41d73b999be6c56962a56d44551
alexandermervar <alexandermervar@gmail.com>
2021-12-06 11:12:29 -0500

    Started work on BlackjackPlayer.java

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/blackjack/BlackjackPlayer.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/blackjack/BlackjackPlayer.java
@@ -1,5 +1,19 @@
 package edu.iu.c212.places.games.blackjack;
 
-public class BlackjackPlayer {
+public class BlackjackPlayer extends BlackJackParticipant{
+
+    public BlackjackPlayer() {
+
+    }
+
+    public String getCurrentTotalsString() {
+        
+    }
+
+    @Override
+    public int getBestTotal() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }
