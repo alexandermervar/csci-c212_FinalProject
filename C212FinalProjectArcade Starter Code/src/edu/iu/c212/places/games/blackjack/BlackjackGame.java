190b4162dc42cf0e91d1be41cdd1a0516053d972
alexandermervar <alexandermervar@gmail.com>
2021-12-06 14:33:21 -0500

    Started work on BackjackGame.java

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/blackjack/BlackjackGame.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/games/blackjack/BlackjackGame.java
@@ -1,19 +1,60 @@
 package edu.iu.c212.places.games.blackjack;
 
+import edu.iu.c212.Arcade;
 import edu.iu.c212.models.User;
+import edu.iu.c212.places.Places;
 import edu.iu.c212.places.games.Game;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class BlackjackGame extends Game {
 
-    public BlackjackGame(String placeName) {
-        setPlaceName(placeName);
-        setEntryFee(0); // Not final value
+    /*
+        The members as described in Lab08 should be included here, as well
+        as player, user, and dealer members
+            - a dealerLabel member that can be updated during events
+              like the player's label
+    */
+
+    public BlackjackGame(Arcade arc) {
+        //TODO
+        // Call the superclass constructor
+        setPlaceName(Places.BLACKJACKGAME);
+        setEntryFee();
+        setPrize();
     }
 
     @Override
     public void onEnter(User user) {
-        // TODO Auto-generated method stub
+        //TODO
+        // Initialize player, dealer, and user members
+        // Set up and display the JFrame as in Lab08
         
+    }
+
+    private class HitButtonListener implements ActionListener {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                // Use you hit() method on the player
+                // Follow other expectations from Lab08
+                
+            }
+            
+    }
+
+    private class StayButtonListener implements ActionListener {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                // Follow other expectations from Lab08
+                // User your play() method on the dealer
+                
+            }
+            
     }
 
     
